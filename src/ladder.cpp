253b4216b97b25e881dd974935a5d8476f279646
slenren <150762646+slenren@users.noreply.github.com>
2025-03-16 23:51:38 -0700

    PLEASE BRO IM COOKED

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -5,22 +5,20 @@ void error(string word1, string word2, string message) {
 }
 
 inline bool edit_distance_within(const string& word1, const string& word2, int d) {
-    if (word1 == word2) return true;
-
     int len1 = word1.size(), len2 = word2.size();
-    if (abs(len1 - len2) > d) return false;
-
+    if (abs(len1 - len2) > 1) return false;
+    
     int i = 0, j = 0, count = 0;
-
     while (i < len1 && j < len2) {
         if (word1[i] != word2[j]) {
-            if (++count > d) return false;
-            (len1 > len2) ? ++i : (len1 < len2) ? ++j : (++i, ++j);
+            if (++count > 1) return false;
+            if (len1 > len2) ++i;
+            else if (len1 < len2) ++j;
+            else { ++i; ++j; }
         } else {
-          ++i; ++j;
+            ++i; ++j;
         }
     }
-
     return true;
 }
 
@@ -30,28 +28,27 @@ inline bool is_adjacent(const string& word1, const string& word2) {
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     if (begin_word == end_word || !word_list.count(end_word)) return {};
-
+    
     queue<vector<string>> ladders;
     unordered_set<string> visited;
-    ladders.emplace(vector<string>{begin_word});
+    ladders.push({begin_word});
     visited.insert(begin_word);
-
+    
     while (!ladders.empty()) {
         int size = ladders.size();
         unordered_set<string> level_visited;
-
+        
         while (size--) {
             vector<string> ladder = std::move(ladders.front());
             ladders.pop();
             const string& last_word = ladder.back();
-
-            if (last_word == end_word) return ladder;
-
-            for (const string& word : word_list) {
+            
+            for (const auto& word : word_list) {
                 if (!visited.count(word) && is_adjacent(last_word, word)) {
                     vector<string> new_ladder = ladder;
-                    new_ladder.emplace_back(word);
-                    ladders.emplace(std::move(new_ladder));
+                    new_ladder.push_back(word);
+                    if (word == end_word) return new_ladder;
+                    ladders.push(std::move(new_ladder));
                     level_visited.insert(word);
                 }
             }
@@ -60,6 +57,7 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
     }
     return {};
 }
+      
 
 void load_words(set<string>& word_list, const string& file_name) {
     ifstream file(file_name);
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -25,13 +25,12 @@ void verify_word_ladder() {
 int main() {
     verify_word_ladder();
 
-    // set<string> word_list;
+    set<string> word_list;
 
-    // load_words(word_list, "/Users/coolkid/Downloads/ics46/ics-46-hw-9-slenren/src/words.txt");
+    load_words(word_list, "/Users/coolkid/Downloads/ics46/ics-46-hw-9-slenren/src/words.txt");
 
-    // vector<string> ladder = generate_word_ladder("were", "were", word_list);
-    // cout << ladder.size();
-    // print_word_ladder(ladder);
-    // cout << generate_word_ladder("were", "were", word_list).size();
+    vector<string> ladder = generate_word_ladder("work", "play", word_list);
+    cout << ladder.size();
+    print_word_ladder(ladder);
 
 }
